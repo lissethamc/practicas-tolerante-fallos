fb5f16c1f8d7c6a2948224f5d380e19473d39968
lissethamc <lis.seth2k16@gmail.com>
2023-02-06 22:18:02 -0600

    bloc funcionando, falta autosave

--- Checkpointing/notePad/main.py
+++ Checkpointing/notePad/main.py
@@ -4,8 +4,18 @@ from mainwindow import MainWindow
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtCore
 
+
+def saveFile():
+    retrievedText = widget.plainTextEdit.toPlainText()
+    print(retrievedText)
+
+    newFile = open("notas.txt","w")
+    newFile.write(retrievedText)
+    newFile.close()
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWindow()
     widget.show()
+    widget.actionGuardar.triggered.connect(saveFile)
     sys.exit(app.exec())

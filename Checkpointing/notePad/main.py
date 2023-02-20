7de6105e3ec41c315848c114668e387751c681dd
lissethamc <lis.seth2k16@gmail.com>
2023-02-19 18:31:54 -0600

    autosaved working

--- Checkpointing/notePad/main.py
+++ Checkpointing/notePad/main.py
@@ -1,8 +1,19 @@
 import sys
+import os
 
 from mainwindow import MainWindow
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtCore
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def autoSave():
+    retrievedText = widget.plainTextEdit.toPlainText()
+    print(retrievedText)
+
+    newFile = open("autosaved.txt","w")
+    newFile.write(retrievedText)
+    newFile.close()
 
 
 def saveFile():
@@ -13,9 +24,20 @@ def saveFile():
     newFile.write(retrievedText)
     newFile.close()
 
+def retrieveText():
+    if os.path.getsize('autosaved.txt') != 0:
+        f = open("autosaved.txt","r")
+        widget.plainTextEdit.setPlainText(f.read())
+        print(f.read()) 
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWindow()
+    retrieveText()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(autoSave, 'interval', seconds=30)
+    scheduler.start()
     widget.show()
     widget.actionGuardar.triggered.connect(saveFile)
     sys.exit(app.exec())
